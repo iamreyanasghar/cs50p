e2fde0ea0d83d41a2b24774cd29b45d6153ae5e0
Reyan Asghar <iamreyanasghar@gmail.com>
2026-05-06 21:38:24 +0500

    coke.py done without AI

--- pset02/coke.py
+++ pset02/coke.py
@@ -2,12 +2,14 @@ amount_due = 50
 
 while amount_due > 0:
     print(f"Amount Due: {amount_due}")
-    
-    coin = int(input("Insert Coin: "))
-    
-    if coin in [25, 10, 5]:
-        amount_due -= coin
 
-# Calculate change (if amount goes below 0)
-change_owed = abs(amount_due)
-print(f"Change Owed: {change_owed}")
+    coin_inserted = int(input("Insert Coin: "))
+    if coin_inserted in [25, 10, 5]:
+        amount_due -= coin_inserted
+        continue
+    else:
+        print(f"Amount_due: {amount_due}")
+
+
+change = abs(amount_due)
+print(f"Change Owed: {change}")
